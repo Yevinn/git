c38634da5287b8b5f69a4db8ee01a21ba73b2ae2
Yevinn <kevin@yevinn.com>
2017-07-25 14:31:19 -0600

    Random Number Challenge, Part II
    Proactively deal with problems with the random number function.

--- learning_JavaScript/random.js
+++ learning_JavaScript/random.js
@@ -1,7 +1,15 @@
 function getRandomNumber(upper, lower) {
-  var randomNumber = Math.floor(Math.random() * (upper - lower + 1)) + lower; 
-    console.log(upper, lower);
-  return randomNumber;
+    if ( isNaN(upper) || isNaN(lower) ){
+        throw new Error("One of your values is a string not a number");
+        }else{
+            var randomNumber = Math.floor(Math.random() * (upper - lower + 1)) + lower; 
+            return randomNumber;
+        }
+  
 }
 
 console.log(getRandomNumber(53, 3));
+console.log(getRandomNumber('nine', 24));
+console.log(getRandomNumber(200, 74637));
+console.log(getRandomNumber(1000, 20000));
+console.log(getRandomNumber(50, 'one-hundred'));
