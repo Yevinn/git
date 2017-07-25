5a8ad1de0c99e866259725350801e8ae172fbfe8
Yevinn <kevin@yevinn.com>
2017-07-25 14:10:43 -0600

    Random Number Challenge Solution
    forgot to make + lower number instead of 1

--- learning_JavaScript/random.js
+++ learning_JavaScript/random.js
@@ -1,5 +1,5 @@
 function getRandomNumber(upper, lower) {
-  var randomNumber = Math.floor(Math.random() * (upper - lower + 1)) + 1; 
+  var randomNumber = Math.floor(Math.random() * (upper - lower + 1)) + lower; 
     console.log(upper, lower);
   return randomNumber;
 }
