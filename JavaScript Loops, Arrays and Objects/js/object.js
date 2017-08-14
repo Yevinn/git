269264eea6a988d4355cc25b032a85a6f6eb13bb
Yevinn <kevin@yevinn.com>
2017-08-14 11:33:21 -0600

    Setting up - Using `for in` to Loop Through an Object's Properties

--- JavaScript Loops, Arrays and Objects/js/object.js
+++ JavaScript Loops, Arrays and Objects/js/object.js
@@ -1,4 +1,7 @@
-var student = {
-  name: "Dave",
-  grades: [80, 85, 90, 95]
-}
+var person = {
+  name : 'Sarah',
+  country : 'US',
+  age : 35,
+  treehouseStudent : true,
+  skills : ['JavaScript', 'HTML', 'CSS']
+};
