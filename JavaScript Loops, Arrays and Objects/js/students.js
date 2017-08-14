2f8c1e646c3a63ac9085f7158b179d8076db407c
Yevinn <kevin@yevinn.com>
2017-08-14 13:31:34 -0600

    My Solution

---
+++ JavaScript Loops, Arrays and Objects/js/students.js
@@ -0,0 +1,32 @@
+var students = [
+  {
+    name: 'Stink Pete',
+    track: 'iOS',
+    achievements: 45,
+    points: 32,
+  },
+  {
+    name: 'Orange Face',
+    track: 'Front End Development',
+    achievements: 4,
+    points: 320,
+  },
+  {
+    name: 'Steve the Pirate',
+    track: 'Web Design',
+    achievements: 475,
+    points: 32342,
+  },
+  {
+    name: 'The Big Stink Face',
+    track: 'Front End Development',
+    achievements: 412,
+    points: 2235,
+  },
+  {
+    name: 'The Old Woman',
+    track: 'Wasting Timet',
+    achievements: 9999,
+    points: 9999,
+  }
+]
