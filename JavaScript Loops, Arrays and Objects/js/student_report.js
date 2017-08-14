d04c82cc786cf9331c8a240d1123809d603589fa
Yevinn <kevin@yevinn.com>
2017-08-14 14:27:56 -0600

    Probably Not the best sollution but here is my Solution

--- JavaScript Loops, Arrays and Objects/js/student_report.js
+++ JavaScript Loops, Arrays and Objects/js/student_report.js
@@ -1,16 +1,30 @@
 var message = '';
 var student;
-
+var search;
 function print(messag){
   var outputDiv = document.getElementById('output');
   outputDiv.innerHTML = message;
 }
 
-for(var i = 0; i < students.length; i+=1){
-  student = students[i];
-  message += '<h2>Student: ' + student.name + '</h2>';
-  message+= '<p>Track: ' + student.track + '</p>';
-  message+= '<p>Achievement: ' + student.achievements + '</p>';
-  message+='<p>Scores: ' + student.scores + '</p>';
+while (true){
+  search = prompt("Search student records: type a name [Jody] (or type 'quit' to end)")
+  if ( search === 'quit'){
+    break;
+  }else{
+    for(var i = 0; i < students.length; i+=1){
+      if(search === students[i].name){
+        student = students[i];
+        message += '<h2>Student: ' + student.name + '</h2>';
+        message+= '<p>Track: ' + student.track + '</p>';
+        message+= '<p>Achievement: ' + student.achievements + '</p>';
+        message+='<p>Scores: ' + student.points + '</p>';
+        break;
+      }else {
+        message += '<p>The search ' + search + ' didn\'t load anything.</p>'
+      }
+    }
+  }
 }
+
+
 print(message);
